56373d9411667470b001540896ff83364a0375a5
Dry-Eggo <abdulewenyi3@gmail.com>
2025-08-04 21:21:45 +0100

    initial

--- src/frontend/mod.rs
+++
@@ -1,6 +0,0 @@
-pub(crate) mod lexer;
-pub(crate) mod parser;
-pub(crate) mod node;
-pub(crate) mod token;
-pub(crate) mod driver;
-pub(crate) mod span;
--- src/frontend/node.rs
+++
@@ -1,8 +0,0 @@
-#![allow(unused)]
-
-use crate::frontend:: {span::Spanned};
-
-#[derive(Debug, Clone)]
-pub enum Node<'a> {
-    Program (Vec<Spanned<'a, Box<Node<'a>>>>)
-}
